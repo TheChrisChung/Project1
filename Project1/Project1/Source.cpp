f4453e7a8720e28f32d3e335f31a47abbd8d52be
TheChrisChung-PC <thechrischung96@gmail.com>
2017-10-15 23:11:42 +0200

    11:11 this is a commit

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	int maxN = n;
+
+	for (; n > 0; --n) {
+		cout << n << " ";
+	}
+	for (; n <= maxN; ++n) {
+		cout << n << " ";
+	}
+
+	cout << endl;
+	int m;
+	cout << "Enter a number: ";
+	cin >> m;
+	int maxM = m;
+
+	for (; m > 0; --m) {
+		cout << m << " ";
+	}
+	for (; m <= maxM; ++m) {
+		cout << m << " ";
+	}
+
+	return 0;
+	// test
+}
